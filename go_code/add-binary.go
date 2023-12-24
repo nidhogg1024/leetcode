be88ea8a2bd70988a97aa5a9cfd6a11bc03305db
nidhogg <360932425@qq.com>
2023-12-25 00:14:58 +0800

    feat:2023/12/24 二进制求和

--- go_code/add-binary.go
+++ go_code/add-binary.go
@@ -1,1 +1,24 @@
 package go_code
+
+// https://leetcode.cn/problems/add-binary/
+
+import "strconv"
+
+func addBinary(a string, b string) string {
+	ans := ""
+	carry := 0
+	for i, j := len(a)-1, len(b)-1; i >= 0 || j >= 0; i, j = i-1, j-1 {
+		if i >= 0 {
+			carry += int(a[i] - '0')
+		}
+		if j >= 0 {
+			carry += int(b[j] - '0')
+		}
+		ans = strconv.Itoa(carry%2) + ans
+		carry /= 2
+	}
+	if carry > 0 {
+		ans = "1" + ans
+	}
+	return ans
+}
